3fb5cea39424ff8af47eb0b98e2717678ae16c02
nitin muteja <nitinmuteja@gmail.com>
2022-01-26 23:29:27 +0530

    delete synonym map

--- src/main/kotlin/com/nitin/web/congitivesearch/controllers/SynonymMapController.kt
+++ src/main/kotlin/com/nitin/web/congitivesearch/controllers/SynonymMapController.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import com.azure.search.documents.indexes.SearchIndexClient
 import com.azure.search.documents.indexes.SearchIndexClientBuilder
@@ -17,21 +18,30 @@ import com.nitin.web.congitivesearch.models.Dataset
 @Controller
 @RequestMapping("/api/synonymMap")
 class SynonymMapController(@Value("\${cognitivesearch.url}") private val endpoint: String,@Value("\${cognitivesearch.apikey}") private val  apiKey: String){
+    
+    val client: SearchIndexClient = SearchIndexClientBuilder()
+    .endpoint(endpoint)
+    .credential(AzureKeyCredential(apiKey))
+    .buildClient()
 
+    val synonymMapName="default"
 
     @PostMapping
     fun synonymMap(): ResponseEntity<String> {
-        val client: SearchIndexClient = SearchIndexClientBuilder()
-            .endpoint(endpoint)
-            .credential(AzureKeyCredential(apiKey))
-            .buildClient()
-
-         val synonymMap =  SynonymMap("default",
+      
+         val synonymMap =  SynonymMap(synonymMapName,
             "user, customer\nlogin,signin\nnik=>nicolas");
             client.createSynonymMap(synonymMap);
 
 
         return ResponseEntity.ok("ok")
+    }
+
+    @DeleteMapping
+    fun deleteSynonymMap(): ResponseEntity<String> {
+      
+            client.deleteSynonymMap(synonymMapName);
+        return ResponseEntity.ok("deleted")
     }
 
 }
